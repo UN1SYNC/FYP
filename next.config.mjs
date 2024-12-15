458e76dfa93c08217330685ce3824c7aa5c1e9b3
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2024-12-15 13:32:07 +0500

    setup basic structure to use supabase and folder for components

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      serverActions: {
+        allowedOrigins: [
+          "localhost:3000"
+        ],
+      },
+    },
+    typescript: {
+      ignoreBuildErrors: true, // Ignore TypeScript errors during the build
+    },
+    eslint: {
+      ignoreDuringBuilds: true, 
+    },
+    images: {
+      domains: ['https://hocaqanruaizkgqriodh.supabase.co'],
+    }
+    
+  };
+  
+  export default nextConfig;
