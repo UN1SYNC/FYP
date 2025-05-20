e5f21179ea2ad9da55ec3128742c7925cff7eeff
AllahRakha1234 <allahrakhahsp1234@gmail.com>
2025-05-21 01:14:01 +0500

    fix/build issue

--- app/config/systems/[name]/page.tsx
+++ app/config/systems/[name]/page.tsx
@@ -1,14 +1,18 @@
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 import React from 'react'
 
-const page = () => {
+interface PageProps {
+  params: {
+    name: string
+  }
+}
 
-    const router = useRouter()
-    const { name } = router.query
+const Page = ({ params }: PageProps) => {
+  const { name } = params
+  
   return (
     <div>University details configurations {name} <Link href={`/config/systems`}>Next</Link></div>
   )
 }
 
-export default page
+export default Page
